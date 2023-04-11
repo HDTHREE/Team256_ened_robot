ebc5239790cdba72a228bac353d8a875e05ac2ec
Hayden Dennis <hd3nnis4@gmail.com>
2023-04-11 18:18:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,22 +10,23 @@ rightPort=OUTPUT_B
 gyroPort=INPUT_1
 distConst=0
 
+gyro = GyroSensor(gyroPort)
+gyro.calibrate()
+pos=0,0,0 #x,y,theta(degrees)
+gyro.reset()
+
 m1 = LargeMotor(leftPort)
 m2 = LargeMotor(rightPort)
 tankV = MoveTank(leftPort,rightPort)
-
-gyro = GyroSensor(gyroPort)
-gyro.calibrate()
-pos=0,0,0 #x,y,theta
-gyro.reset()
+tankV.gyro=gyro
 
 diameter=2.2*2.54 #this value represents the diameter of the wheels in cm
 circ = pi*diameter
-def MoveDistance(n): 
+def MoveDistance(m):
+    n=m/2 
     global pos
     global gyro
     global tankV
-    global distConst
     
     #calculate the distance each wheel needs to travel
     wheelTheta = (n/(diameter/2))*(180/pi)
@@ -38,16 +39,48 @@ def MoveDistance(n):
     tankV.on_for_degrees(SpeedPercent(-20),SpeedPercent(-20), wheelTheta, brake=True, block=True)
     
     #calculate the distance traveled using the average of the two wheel positions
-    posChange = ((m1.position + m2.position) / 2) * circ / 360
-    
+    posChange = ((m1.position + m2.position) / 2) * circ
+    print(posChange) 
+    #should return value close to m
+
     #update the position and heading using the gyro sensor
     heading = gyro.angle
-    posX = pos[0] + (posChange + distConst) * cos(heading * pi / 180)
-    posY = pos[1] + (posChange + distConst) * sin(heading * pi / 180)
-    pos = posX, posY
+    posX = pos[0] + (posChange) * cos(heading * (pi / 180))
+    posY = pos[1] + (posChange) * sin(heading * (pi / 180))
+    pos = posX, posY, (heading%360)
     
     #reset the gyro sensor for the next movement
     gyro.reset()
 
+def TurnAngle(d):
+    global pos
+    global gyro
+    global tankV
+
+    gyro.reset
+
+    tankV.turn_degrees(SpeedPercent(10), d, brake=True, error_margin=1, sleep_time=0.01)
+
+    pos[2] = (pos[2] + d)%360
+
+    gyro.reset
+
+class Subtasks:
+    def Subtask1(x):
+        MoveDistance(24)
+        TurnAngle(-90)
+        MoveDistance(x)
+        sleep(5)
+        MoveDistance(96-x)
+        TurnAngle(-90)
+        MoveDistance(24)
+    def Subtask2():
+        TurnAngle(180)
+        MoveDistance(6)
+        TurnAngle(90)
+        MoveDistance(96)
+        TurnAngle(90)
+        MoveDistance(6)
+    
 if __name__ == "__main__":
-    MoveDistance(2.4)
+    TurnAngle(-90)
