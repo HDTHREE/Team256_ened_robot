b83c8a38e5382284865e979e4d3c4dfb80e61147
Hayden Dennis <hd3nnis4@gmail.com>
2023-04-11 19:07:09 -0400

    Subtasks py files

---
+++ Subtask1.py
@@ -0,0 +1,4 @@
+from main import Subtasks
+import main
+
+Subtasks.Subtask1(x)
---
+++ Subtask3.py
@@ -0,0 +1,5 @@
+from main import Subtasks
+import main
+inputCode=0 #binary value 0-15
+
+Subtasks.Subtask1(inputCode)
