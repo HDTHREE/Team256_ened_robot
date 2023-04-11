0760f78ebe4aadc74c625f36306e979570abcad6
Hayden Dennis <hd3nnis4@gmail.com>
2023-04-11 19:03:38 -0400

    Finish new testing class

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
 from ev3dev2.motor import *
 from ev3dev2.sensor import *
-from ev3dev2.sensor.lego import GyroSensor
+from ev3dev2.display import *
+from ev3dev2.sensor.lego import GyroSensor, ColorSensor
 from time import sleep
 from math import pi, sin, cos
 
 leftPort=OUTPUT_A
 rightPort=OUTPUT_B
+mediumPort=OUTPUT_D
 gyroPort=INPUT_1
 distConst=0
+colorPort=INPUT_3
+liftVal=30
 
 gyro = GyroSensor(gyroPort)
 gyro.calibrate()
 pos=0,0,0 #x,y,theta(degrees)
 gyro.reset()
 
+reader = ColorSensor(colorPort)
+
 m1 = LargeMotor(leftPort)
 m2 = LargeMotor(rightPort)
+m3 = MediumMotor(OUTPUT_D)
 tankV = MoveTank(leftPort,rightPort)
 tankV.gyro=gyro
 
@@ -24,6 +31,8 @@ diameter=2.2*2.54 #this value represents the diameter of the wheels in cm
 circ = pi*diameter
 def MoveDistance(m):
     n=m/2 
+    global m1
+    global m2
     global pos
     global gyro
     global tankV
@@ -53,6 +62,8 @@ def MoveDistance(m):
     gyro.reset()
 
 def TurnAngle(d):
+    global m1
+    global m2
     global pos
     global gyro
     global tankV
@@ -64,6 +75,42 @@ def TurnAngle(d):
     pos[2] = (pos[2] + d)%360
 
     gyro.reset
+
+def ReadBarcode():
+    global reader
+
+    color=[]
+    color[0]=reader.color
+    MoveDistance(0.5)
+    color[1]=reader.color
+    MoveDistance(0.5)
+    color[2]=reader.color
+    MoveDistance(0.5)
+    color[3]=reader.color
+    MoveDistance(0.5)
+
+    for i in range(len(color)):    
+        if(color[i]==1): {}
+        else: color[i]=6
+
+    binInt=0
+    for i in range(len(color)): 
+        if(color[i]==1): 
+            binInt+=(2**i)
+
+    return binInt
+    
+def Lift():
+    global m3
+    global liftVal
+
+    m3.on_for_degrees(SpeedPercent(10), liftVal, brake=True, block=True)
+
+def Drop():
+    global m3
+    global liftVal
+    
+    m3.on_for_degrees(SpeedPercent(10), (0-liftVal), brake=True, block=True)
 
 class Subtasks:
     def Subtask1(x):
@@ -81,6 +128,31 @@ class Subtasks:
         MoveDistance(96)
         TurnAngle(90)
         MoveDistance(6)
-    
+    def Subtask3(testCode):#input as binary interpretation
+        MoveDistance(10)
+        code=ReadBarcode()
+        Display.update()
+        if(code==testCode): Display.text_pixels("CODE MATCHES", clear_screen=True, text_color='black')
+        else: Display.text_pixels("CODE DOESN'T MATCH", clear_screen=True, text_color='black')
+    def Subtask4():
+        #does three left hand turns and sets the robot to the pickup location
+        MoveDistance(6)
+        TurnAngle(90)
+        MoveDistance(3)
+        TurnAngle(90)
+        MoveDistance(6)
+        TurnAngle(90)
+        MoveDistance(3.3)
+
+        #lifts
+        Lift()
+
+        MoveDistance(-1.3)
+        TurnAngle(90)
+        MoveDistance(14)
+
+        #drop
+        Drop()
+
 if __name__ == "__main__":
     TurnAngle(-90)
